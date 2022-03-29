9c5c3ccb4c6beb2e452b6f8fe6a734a8ff159465
Ahmed Qazi <a_q444@outlook.com>
2022-03-29 16:46:30 +0400

    chore: created basic skeleton

---
+++ infra/providers.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = ">= 1.1.3"
+
+  cloud {
+    hostname     = "app.terraform.io"
+    organization = "Alpha-Project"
+
+    workspaces {
+      name = "notejam"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.73.0"
+    }
+  }
+}
