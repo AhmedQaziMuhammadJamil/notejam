e5b2b8e84cd7043ecda7db0a11e789a6bdc1e9d6
Ahmed Qazi <a_q444@outlook.com>
2022-03-29 17:24:17 +0400

    feat: added-vpc

---
+++ infra/modules/vpc/outputs.tf
@@ -0,0 +1,19 @@
+output "out_nl_vpcid" {
+    value = module.notejam_vpc.vpc_id
+    description = "VPC ID"
+}
+
+output "out_nl_privatesubnet" {
+    value = module.notejam_vpc.private_subnets
+    description = "Private Subnets"
+}
+
+output "out_nl_publicsubnet" {
+    value = module.notejam_vpc.public_subnets
+    description = "Public Subnets"
+}
+
+output "out_nl_rdssubnet" {
+    value = module.notejam_vpc.intra_subnets
+    description = "RDS Subnets"
+}
