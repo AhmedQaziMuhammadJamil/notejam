55a1a2f4db7ae4b2b500618c972f5a6a69cdbee5
Ahmed Qazi <a_q444@outlook.com>
2022-03-30 20:43:24 +0400

    chore: added outputs for sg

---
+++ infra/modules/security-groups/outputs.tf
@@ -0,0 +1,12 @@
+output "alb-sg"{
+    value=aws_security_group.alb["NoteJam-Alb-SG"].id
+} 
+
+output "rds-sg" {
+    value=aws_security_group.rds["NoteJam-RDS-SG"].id
+}
+
+output "worker-sg" {
+    value= aws_security_group.worker-node["NoteJam-Worker-SG"].id
+}
+
