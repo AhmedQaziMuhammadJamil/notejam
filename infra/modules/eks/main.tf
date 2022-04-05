ea04fb562f18c0b873dd835f3030b739e8d56838
Ahmed Qazi <93099637+AhmedQaziMuhammadJamil@users.noreply.github.com>
2022-04-06 00:12:34 +0400

    Update main.tf

--- infra/modules/eks/main.tf
+++ infra/modules/eks/main.tf
@@ -235,7 +235,7 @@ provider "kubernetes" {
   host                   = data.aws_eks_cluster.target.endpoint
   token                  = data.aws_eks_cluster_auth.aws_iam_authenticator.token
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.target.certificate_authority[0].data)
-  config_path = file("config")
+  config_path = "config"
 
  
 }
