2d8a773e5993d36dd744f15b2e82bb2dae6fc5af
Ahmed Qazi <a_q444@outlook.com>
2022-04-06 15:43:43 +0400

    added:image refelector

--- infra/modules/eks/variables.tf
+++ infra/modules/eks/variables.tf
@@ -23,6 +23,6 @@ variable "target_path" {
 
 variable "components_extra" {
   type        = list(string)
-  default     =  []
+  default     =  ["image-reflector-controller","image-automation-controller"]
   description = "extra flux components"
 }
