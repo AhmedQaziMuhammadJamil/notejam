23b3a26c6024de00a0b814f2709e9ca23fe1ba63
Ahmed Qazi <a_q444@outlook.com>
2022-04-10 21:26:01 +0400

    removed-flux

--- infra/main.tf
+++ infra/main.tf
@@ -57,7 +57,7 @@ module "mod_eks" {
   github_token = var.github_token
 }
 
-  module "mod_flux" {
+/*   module "mod_flux" {
   source = "./modules/flux"
   cluster_id= module.mod_eks.cluster_id
   cluster_auth = module.mod_eks.cluster_auth
@@ -66,7 +66,7 @@ module "mod_eks" {
   token=module.mod_eks.token
    github_owner = var.github_owner
   github_token = var.github_token
-}  
+}   */
 
 module "mod_github" {
   source = "./modules/github"
