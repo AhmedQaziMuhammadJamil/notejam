c93bc94b240a9567ab4c35e60946a4b136e2bec5
Ahmed Qazi <a_q444@outlook.com>
2022-04-14 10:14:36 +0400

    added-new-value

--- infra/modules/flux/main.tf
+++ infra/modules/flux/main.tf
@@ -220,8 +220,8 @@ resource "kubernetes_secret" "main" {
    identity       = tls_private_key.main.private_key_pem
     "identity.pub" = tls_private_key.main.public_key_pem
     known_hosts    = local.known_hosts 
-  /*   username="git"
-    password=var.github_token */
+     username="git"
+    password=var.github_token 
   }
 } 
 
