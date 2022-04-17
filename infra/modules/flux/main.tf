0c79e053a227c324cd610a4cd2a7d04784b4b127
Ahmed Qazi <a_q444@outlook.com>
2022-04-18 00:31:50 +0400

    removed commits

--- infra/modules/flux/main.tf
+++ infra/modules/flux/main.tf
@@ -11,6 +11,7 @@ provider "kubernetes" {
  host                   = var.host
   token                  = var.token
   cluster_ca_certificate = var.cluster_ca_certificate
+   load_config_file       = false
 }
 
 
@@ -21,27 +22,19 @@ provider "github" {
   token = var.github_token
 }
 
-# SSH
-/* locals {
-  known_hosts = "github.com ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEmKSENjQEezOmxkZMy7opKgwFB9nkt5YRrYMjNuG5N87uRgg6CLrbo5wAdT/y6v0mKV0U2w0WZ2YB/++Tpockg="
-} */
 
-/* resource "tls_private_key" "main" {
-  algorithm   = "ECDSA"
-  ecdsa_curve = "P256"
-}
- */
+
 data "flux_install" "main" {
    /*  depends_on = [module.eks] */
   target_path      = var.target_path
   components_extra = var.components_extra
+  network_policy = false
 
 }
 
 
 ###Deleting resouces in k8s as provider doesn't provide any idempotentancy
 
-# Kubernetes
 resource "kubernetes_namespace" "flux_system" {
 /*   depends_on = [
     module.eks
@@ -75,20 +68,7 @@ resource "kubernetes_namespace" "monitoring" {
     ]
   }
 }
-
-resource "kubernetes_namespace" "operations" {
-/*   depends_on = [
-    module.eks
-  ] */
-  metadata {
-    name = "operations"
-  }
-  lifecycle {
-    ignore_changes = [
-      metadata[0].labels
-    ]
-  }
-}
+  
 data "kubectl_file_documents" "install" {
   content = data.flux_install.main.content
 }
@@ -118,13 +98,13 @@ resource "kubectl_manifest" "apply" {
 
 
 
-/* resource "kubectl_manifest" "sync" {
+resource "kubectl_manifest" "sync" {
 
   
   for_each   = { for v in local.sync : lower(join("/", compact([v.data.apiVersion, v.data.kind, lookup(v.data.metadata, "namespace", ""), v.data.metadata.name]))) => v.content }
   depends_on = [kubernetes_namespace.flux_system]
   yaml_body = each.value
-}  */
+} 
 
 
 ####Flux+GitHub 8-04-2022
@@ -141,36 +121,27 @@ resource "github_branch_default" "main" {
   repository = github_repository.main.name
   branch     = var.branch
 }
-
-/* resource "github_repository_deploy_key" "main" {
-  title      = "staging-cluster"
-  repository = github_repository.main.name
-  key        = tls_private_key.main.public_key_openssh
-  read_only  = true
-} */
-
 resource "github_repository_file" "install" {
   repository = github_repository.main.name
   file       = data.flux_install.main.path
   content    = data.flux_install.main.content
   branch     = var.branch
 }
-
-
-/* resource "github_repository" "sync" {
-  name       = var.sync_repo
-  visibility = var.repository_visibility
-  auto_init  = true
-} */
-
-### Sync
-/* 
+  
 data "flux_sync" "main" {
-  target_path = var.sync_target_path
-  url         = "ssh://git@github.com/${var.github_owner}/${var.sync_repo}.git"
+  target_path = var.target_path
+  url         = local.url
   branch      = var.branch
+  git_implementation = "go-git"
+  name = "notejam-source"
+  secret = "flux-system"
+  namespace = "flux-system"
+  
 }
 
+output "gitrepo" {
+  value= "${data.flux_sync.main}"
+}
 
 data "kubectl_file_documents" "sync" {
   content = data.flux_sync.main.content
@@ -185,67 +156,13 @@ resource "kubernetes_secret" "main" {
   }
  
    data = {
-    identity       = tls_private_key.main.private_key_pem
-    "identity.pub" = tls_private_key.main.public_key_pem
-    known_hosts    = local.known_hosts
-  }
-} 
-
-resource "github_repository_file" "sync" {
-  repository = github_repository.sync.name
-  file       = data.flux_sync.main.path
-  content    = data.flux_sync.main.content
-  branch     = var.branch
-}
-
-resource "github_repository_file" "kustomize" {
-  repository = github_repository.sync.name
-  file       = data.flux_sync.main.kustomize_path
-  content    = data.flux_sync.main.kustomize_content
-  branch     = var.branch
-}
-  */
-
-
-  
-/* data "flux_sync" "main" {
-  target_path = var.target_path
-  url         = local.url
-  branch      = var.branch
-  git_implementation = "go-git"
-  name = "notejam-source"
-  secret = "flux-system"
-  namespace = "flux-system"
-  
-}
- */
-output "gitrepo" {
-  value= "${data.flux_sync.main}"
-}
-
-/* data "kubectl_file_documents" "sync" {
-  content = data.flux_sync.main.content
-} */
-
-resource "kubernetes_secret" "main" {
-  depends_on = [kubectl_manifest.apply]
-
-  metadata {
-    name      = data.flux_sync.main.secret
-    namespace = data.flux_sync.main.namespace
-  }
- 
-   data = {
-/*    identity       = tls_private_key.main.private_key_pem
-    "identity.pub" = tls_private_key.main.public_key_pem
-    known_hosts    = local.known_hosts  */
      username="git"
      password=var.github_token 
   }
 } 
 
 
-/* resource "github_repository_file" "sync" {
+resource "github_repository_file" "sync" {
   repository = github_repository.main.name
   file       = data.flux_sync.main.path
   content    = data.flux_sync.main.content
@@ -258,4 +175,3 @@ resource "github_repository_file" "kustomize" {
   content    = data.flux_sync.main.kustomize_content
   branch     = var.branch
 }
- */
