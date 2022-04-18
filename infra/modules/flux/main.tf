c69ba4d8154ee140362229b547d7619647543869
Ahmed Qazi <a_q444@outlook.com>
2022-04-18 18:08:33 +0400

    new-2

--- infra/modules/flux/main.tf
+++ infra/modules/flux/main.tf
@@ -54,14 +54,17 @@ resource "kubernetes_namespace" "flux_system" {
       metadata[0].labels
     ]
   }
-     provisioner "local-exec" {
-      command = "curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && chmod +x kubectl"
-    }
     provisioner "local-exec" {
     when       = destroy
-    command    = "kubectl  patch    customresourcedefinition helmcharts.source.toolkit.fluxcd.io  gitrepositories.source.toolkit.fluxcd.io  securitygrouppolicies.vpcresources.k8s.aws helmreleases.helm.toolkit.fluxcd.io helmrepositories.source.toolkit.fluxcd.io kustomizations.kustomize.toolkit.fluxcd.io kustomizations.kustomize.toolkit.fluxcd.io -p '{\"metadata\":{\"finalizers\":null}}'"
+    command    = <<-EOT
+      "curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && chmod +x kubectl"
+      "kubectl get namespace flux-system -o json > flux-system.json"
+      "kubectl patch customresourcedefinition helmcharts.source.toolkit.fluxcd.io helmreleases.helm.toolkit.fluxcd.io helmrepositories.source.toolkit.fluxcd.io kustomizations.kustomize.toolkit.fluxcd.io gitrepositories.source.toolkit.fluxcd.io -p '{\"metadata\":{\"finalizers\":null}}'"
+    EOT
     on_failure = continue
   }
+
+  
  
 }
 
