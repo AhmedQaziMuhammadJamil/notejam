67483150ec2739e6453361328c7ddddfd0d512ba
Ahmed Qazi <a_q444@outlook.com>
2022-04-18 18:15:01 +0400

    new-3

--- infra/modules/flux/main.tf
+++ infra/modules/flux/main.tf
@@ -59,7 +59,7 @@ resource "kubernetes_namespace" "flux_system" {
     command    = <<-EOT
       "curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && chmod +x kubectl"
       "kubectl get namespace flux-system -o json > flux-system.json"
-      "kubectl patch customresourcedefinition helmcharts.source.toolkit.fluxcd.io helmreleases.helm.toolkit.fluxcd.io helmrepositories.source.toolkit.fluxcd.io kustomizations.kustomize.toolkit.fluxcd.io gitrepositories.source.toolkit.fluxcd.io -p '{\"metadata\":{\"finalizers\":null}}'"
+      "kubectl patch customresourcedefinition helmcharts.source.toolkit.fluxcd.io helmreleases.helm.toolkit.fluxcd.io helmrepositories.source.toolkit.fluxcd.io kustomizations.kustomize.toolkit.fluxcd.io gitrepositories.source.toolkit.fluxcd.io -p '{\"metadata\":{"finalizers":null}}'"
     EOT
     on_failure = continue
   }
