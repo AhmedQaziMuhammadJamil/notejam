e1679e88eb929efb5a49700a61411c108654cd30
Ahmed Qazi <a_q444@outlook.com>
2022-04-24 00:54:16 +0400

    alb-sg

--- infra/modules/eks/main.tf
+++ infra/modules/eks/main.tf
@@ -221,6 +221,14 @@ module "eks" {
     to_port     = 8443
     type        = "ingress"
   }
+   ingress_allow_access_from_control_plane = {
+      type                          = "ingress"
+      protocol                      = "tcp"
+      from_port                     = 9443
+      to_port                       = 9443
+      source_cluster_security_group = true
+      description                   = "Allow access from control plane to webhook port of AWS load balancer controller"
+    }
   }
 
    cluster_security_group_additional_rules = {
