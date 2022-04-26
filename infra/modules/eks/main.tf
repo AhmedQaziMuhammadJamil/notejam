c2c5ede219179999ba7e775d8003bd5516356bc7
Ahmed Qazi <a_q444@outlook.com>
2022-04-26 21:16:56 +0400

    local-exec-fix

--- infra/modules/eks/main.tf
+++ infra/modules/eks/main.tf
@@ -382,7 +382,6 @@ ${self.triggers.ca_crt}
 EOF
 curl -o aws-iam-authenticator https://amazon-eks.s3.us-west-2.amazonaws.com/1.17.9/2020-08-04/bin/linux/amd64/aws-iam-authenticator && chmod +x ./aws-iam-authenticator && \
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && chmod +x ./kubectl && \
-kubectl version --client
 mkdir -p $HOME/bin && mv ./aws-iam-authenticator $HOME/bin/ && export PATH=$PATH:$HOME/bin && \
 ./kubectl \
   --server="${self.triggers.endpoint}" \
