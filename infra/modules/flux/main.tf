418c018ddbfe9916bce9f199666d6d5d9c88b891
Ahmed Qazi <a_q444@outlook.com>
2022-04-26 17:56:23 +0400

    removed-depends on

--- infra/modules/flux/main.tf
+++ infra/modules/flux/main.tf
@@ -145,7 +145,7 @@ EOT
 resource "kubectl_manifest" "apply" {
 
   for_each   = { for v in local.install : lower(join("/", compact([v.data.apiVersion, v.data.kind, lookup(v.data.metadata, "namespace", ""), v.data.metadata.name]))) => v.content }
-  depends_on = [kubernetes_namespace.flux_system]
+  //depends_on = [kubernetes_namespace.flux_system]
   yaml_body = each.value
 }
 
@@ -154,7 +154,7 @@ resource "kubectl_manifest" "apply" {
 resource "kubectl_manifest" "sync" {
 
   for_each   = { for v in local.sync : lower(join("/", compact([v.data.apiVersion, v.data.kind, lookup(v.data.metadata, "namespace", ""), v.data.metadata.name]))) => v.content }
-  depends_on = [kubernetes_namespace.flux_system]
+  //depends_on = [kubernetes_namespace.flux_system]
   yaml_body = each.value
  
 }
