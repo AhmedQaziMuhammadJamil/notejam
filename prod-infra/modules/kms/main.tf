8630fc58c1a4fa297077ef1c702c2b2763df89b3
Ahmed Qazi <a_q444@outlook.com>
2022-05-06 23:01:46 +0400

    changed kms deletion days

--- prod-infra/modules/kms/main.tf
+++ prod-infra/modules/kms/main.tf
@@ -5,7 +5,7 @@ module "rds_kms_key" {
   enabled                 = true
   name                    = local.rds_key_name
   description             = "KMS key for RDS"
-  deletion_window_in_days = 0
+  deletion_window_in_days = 7
   enable_key_rotation     = true
   alias                   = "alias/rds-cmk-${var.env}"
   multi_region            = true
@@ -20,7 +20,7 @@ module "eks_kms_key" {
   enabled                 = true
   name                    = local.eks_key_name
   description             = "KMS key for EKS"
-  deletion_window_in_days = 0
+  deletion_window_in_days = 7
   enable_key_rotation     = true
   alias                   = "alias/eks-cmk-${var.env}"
   multi_region            = true
@@ -34,7 +34,7 @@ module "s3_kms_key" {
   enabled                 = true
   name                    = local.s3_key_name
   description             = "KMS key for S3 objects"
-  deletion_window_in_days = 0
+  deletion_window_in_days = 7
   enable_key_rotation     = true
   alias                   = "alias/s3-cmk-${var.env}"
   multi_region            = true
