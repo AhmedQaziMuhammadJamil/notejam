7301cd864e75ef29d0c404adf47947f7529668b9
Ahmed Qazi <a_q444@outlook.com>
2022-05-07 04:58:56 +0400

    reverted-values

--- prod-infra/modules/waf/locals.tf
+++ prod-infra/modules/waf/locals.tf
@@ -2,44 +2,44 @@ locals {
   waf_name = "${var.waf_name}-${var.env}"
   ip_sets = {
     whitelist_ipv4 = {
-      ip_address_version = "IPV4-${var.env}"
+      ip_address_version = "IPV4"
       description        = "Allow whitelist for IPV4 addresses"
     }
     whitelist_ipv6 = {
-      ip_address_version = "IPV6-${var.env}"
+      ip_address_version = "IPV6"
       description        = "Allow whitelist for IPV6 addresses"
     }
     scanners_probes_ipv6 = {
-      ip_address_version = "IPV6-${var.env}"
+      ip_address_version = "IPV6"
       description        = "Block Scanners/Probes IPV6 addresses"
     }
     scanners_probes_ipv4 = {
-      ip_address_version = "IPV4-${var.env}"
+      ip_address_version = "IPV4"
       description        = "Block Scanners/Probes IPV4 addresses"
     }
     ipreputationlist_ipv6 = {
-      ip_address_version = "IPV6-${var.env}"
+      ip_address_version = "IPV6"
       description        = "Block Reputation List IPV6 addresses"
     }
     ipreputationlist_ipv4 = {
-      ip_address_version = "IPV4-${var.env}"
+      ip_address_version = "IPV4"
       description        = "Block Reputation List IPV4 addresses"
     }
     ip_badbot_ipv6 = {
-      ip_address_version = "IPV6-${var.env}"
+      ip_address_version = "IPV6"
       description        = "Block Bad Bot IPV6 addresses"
     }
     ip_badbot_ipv4 = {
-      ip_address_version = "IPV4-${var.env}"
+      ip_address_version = "IPV4"
       description        = "Block Bad Bot IPV4 addresses"
     }
 
     blacklist_ipv6 = {
-      ip_address_version = "IPV6-${var.env}"
+      ip_address_version = "IPV6"
       description        = "Block blacklist for IPV6 addresses"
     }
     blacklist_ipv6 = {
-      ip_address_version = "IPV4-${var.env}"
+      ip_address_version = "IPV4"
       description        = "Block blacklist for IPV4 addresses"
     }
   }
