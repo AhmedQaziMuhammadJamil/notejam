ec08295cdaf17d5c47780902ace52817db8b077f
Ahmed Qazi <a_q444@outlook.com>
2022-05-07 21:38:47 +0400

    removed unneccessary variables

--- infra/modules/eks/variables.tf
+++ infra/modules/eks/variables.tf
@@ -14,41 +14,6 @@ variable "private_subnets" {
 variable "worker-sg" {
 
 }
-##########################flux####################33
-variable "target_path" {
-  type        = string
-  default     = "notejam"
-  description = "flux install target path"
-}
-
-variable "components_extra" {
-  type        = list(string)
-  default     =  ["image-reflector-controller","image-automation-controller"]
-  description = "extra flux components"
-}
-variable "github_owner" {
-  type = string
-}
-variable "github_token" {
-  type = string
-}
-variable "repository_name" {
-  type        = string
-  default     = "flux-aq"
-  description = "github repository name"
-}
-
-variable "repository_visibility" {
-  type        = string
-  default     = "private"
-  description = "How visible is the github repo"
-}
-
-variable "branch" {
-  type        = string
-  default     = "main"
-  description = "branch name"
-}
 
 variable "env" {
   
