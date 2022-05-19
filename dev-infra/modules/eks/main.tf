1ba53d85a1776525b83f564a632fb4c05867f594
Ahmed Qazi <a_q444@outlook.com>
2022-05-19 18:21:46 +0400

    config

--- dev-infra/modules/eks/main.tf
+++ dev-infra/modules/eks/main.tf
@@ -315,6 +315,7 @@ provider "helm" {
     host                   = data.aws_eks_cluster.target.endpoint
     token                  = data.aws_eks_cluster_auth.aws_iam_authenticator.token
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.target.certificate_authority[0].data)
+     load_config_file       = false
     
   }
 }
