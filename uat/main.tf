f11a4d1808d8517b1474dc389311bdae1b62bb8a
Ahmed Qazi <a_q444@outlook.com>
2022-07-14 16:16:15 +0400

    changes

--- uat/main.tf
+++ uat/main.tf
@@ -106,7 +106,7 @@ module "mq_broker" {
     name                       = "mq-broker"
     apply_immediately          = true
     auto_minor_version_upgrade = true
-    deployment_mode            = "SINGLE_INSTANCE"
+    deployment_mode            = "CLUSTER_MULTI_AZ"
     engine_type                = "RabbitMQ"
     engine_version             = "3.8.6"
     host_instance_type         = "mq.m5.large"
