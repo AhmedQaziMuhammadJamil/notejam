b0de2c3c34fb605f3d8a81d416c7a49f77a6fa76
Ahmed Qazi <a_q444@outlook.com>
2022-07-18 04:00:20 +0400

    new-changes

--- uat/outputs.tf
+++ uat/outputs.tf
@@ -2,6 +2,6 @@ output "account-id" {
   value = data.aws_caller_identity.current.account_id
 }
 
-output "zones-id" {
+/* output "zones-id" {
   value = module.route53_zones.route53_zone_zone_id
-}
+} */
