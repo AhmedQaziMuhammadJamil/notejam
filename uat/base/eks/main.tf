61b480f494b739f9f8ad991931b9b27e5da9537c
Ahmed Qazi <a_q444@outlook.com>
2022-07-18 12:46:34 +0400

    changes

--- uat/base/eks/main.tf
+++ uat/base/eks/main.tf
@@ -121,7 +121,6 @@ provider "kubernetes" {
   host                   = data.aws_eks_cluster.default.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.default.token
-   load_config_file       = false
 }
 provider "helm" {
   kubernetes {
